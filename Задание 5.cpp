5a039e818ad129a044ea3079bd00fbdc3d2351dc
Mari-Sokol <63508279+Mari-Sokol@users.noreply.github.com>
2020-05-01 02:03:07 +0800

    Add files via upload

---
+++ Задание 5.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    int x;
+    cout<< "¬ведите х:";
+    cin>>x;
+    double Y=x;
+    double Yi=0;
+    double Yabs=0;
+    double endcalc=1.0e-5;
+    do
+    {
+    Yi=0.5*(Y+(3*x/(2*Y*Y+x/Y)));
+    Yabs=abs(Yi-Y);
+    Y=Yi;
+    }
+    while(Yabs>endcalc);
+    cout<< endl << Y << endl;
+    return 0;
+}
